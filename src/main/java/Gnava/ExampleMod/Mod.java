f7d2f3fcbcedea4193fd8429da4d59e20201ddb8
Michał <m12@onet.com.pl>
2026-07-26 12:17:33 +0200

    init

---
+++ src/main/java/Gnava/ExampleMod/Mod.java
@@ -0,0 +1,11 @@
+package Gnava.ExampleMod;
+
+import Gnava.ModApi.IMod;
+import Gnava.ModApi.IModContext;
+
+public class Mod implements IMod {
+    @Override
+    public void initialise(IModContext iModContext) {
+
+    }
+}
